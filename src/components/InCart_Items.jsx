6b8e9656f3c3572b43e9c1b1de3d283b68b45c8a
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-06-23 14:35:51 +0300

    Add cart page main structure

---
+++ src/components/InCart_Items.jsx
@@ -0,0 +1,132 @@
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { Divider, Grid, IconButton, Stack, Typography } from "@mui/material";
+import pasta from "../assets/products/spaghettipasta.jpg";
+
+const InCart_Items = () => {
+  return (
+    <Grid container pt={2} gap={2}>
+      <Grid item width="100%">
+        <Stack direction="row" gap={5}>
+          <Stack
+            direction="row"
+            columnGap={2}
+            maxWidth={320}
+            width="fit-content"
+          >
+            <img src={pasta} alt="pasta" width={85} />
+            <Stack direction="column">
+              <Typography variant="body1">Pasta</Typography>
+              <Typography variant="caption">
+                Durum wheat pasta that cooks to al dente perfection.
+              </Typography>
+              <Typography variant="body2" color="#ed6c02">
+                $4.5
+              </Typography>
+            </Stack>
+          </Stack>
+          <Stack direction="column">
+            <Typography variant="h6" color="#ed6c02">
+              2 In Cart
+            </Typography>
+            <Typography variant="h6" color="#ed6c02">
+              Price: $9.00
+            </Typography>
+            <Stack direction="row" width="fit-content">
+              <IconButton aria-label="remove 1 item from cart" color="inherit">
+                <RemoveIcon />
+              </IconButton>
+              <IconButton aria-label="add 1 item to cart" color="inherit">
+                <AddIcon />
+              </IconButton>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Divider />
+      </Grid>
+      <Grid item width="100%">
+        <Stack direction="row" gap={5}>
+          <Stack
+            direction="row"
+            columnGap={2}
+            maxWidth={320}
+            width="fit-content"
+          >
+            <img src={pasta} alt="pasta" width={85} />
+            <Stack direction="column">
+              <Typography variant="body1">Pasta</Typography>
+              <Typography variant="caption">
+                Durum wheat pasta that cooks to al dente perfection.
+              </Typography>
+              <Typography variant="body2" color="#ed6c02">
+                $4.5
+              </Typography>
+            </Stack>
+          </Stack>
+          <Stack direction="column">
+            <Typography variant="h6" color="#ed6c02">
+              2 In Cart
+            </Typography>
+            <Typography variant="h6" color="#ed6c02">
+              Price: $9.00
+            </Typography>
+            <Stack direction="row" width="fit-content">
+              <IconButton aria-label="remove 1 item from cart" color="inherit">
+                <RemoveIcon />
+              </IconButton>
+              <IconButton aria-label="add 1 item to cart" color="inherit">
+                <AddIcon />
+              </IconButton>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Divider />
+      </Grid>
+      <Grid item width="100%">
+        <Stack direction="row" gap={5}>
+          <Stack
+            direction="row"
+            columnGap={2}
+            maxWidth={320}
+            width="fit-content"
+          >
+            <img src={pasta} alt="pasta" width={85} />
+            <Stack direction="column">
+              <Typography variant="body1">Pasta</Typography>
+              <Typography variant="caption">
+                Durum wheat pasta that cooks to al dente perfection.
+              </Typography>
+              <Typography variant="body2" color="#ed6c02">
+                $4.5
+              </Typography>
+            </Stack>
+          </Stack>
+          <Stack direction="column">
+            <Typography variant="h6" color="#ed6c02">
+              2 In Cart
+            </Typography>
+            <Typography variant="h6" color="#ed6c02">
+              Price: $9.00
+            </Typography>
+            <Stack direction="row" width="fit-content">
+              <IconButton aria-label="remove 1 item from cart" color="inherit">
+                <RemoveIcon />
+              </IconButton>
+              <IconButton aria-label="add 1 item to cart" color="inherit">
+                <AddIcon />
+              </IconButton>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Divider />
+      </Grid>
+      <Grid item>
+        <Typography variant="h5" color="#ed6c02">
+          Total Amount: $27.00
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default InCart_Items;
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,5 +1,18 @@
+import { Paper, Typography } from "@mui/material";
+import InCart_Items from "../components/InCart_Items";
+
 const CartPage = () => {
-  return <div>CartPage</div>;
+  return (
+    <Paper sx={{ p: 3 }}>
+      <Typography variant="h4" gutterBottom>
+        Shopping Cart
+      </Typography>
+      <Typography variant="h6" color="#ed6c02" gutterBottom>
+        Items Added
+      </Typography>
+      <InCart_Items />
+    </Paper>
+  );
 };
 
 export default CartPage;
