fcab6b89ad002e6b88da9ff9650f4891f6cd9b59
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-07-08 09:19:47 +0300

    Refactor Code - wrap quantity select component inside Form Control

--- src/components/QuantitySelect.jsx
+++ src/components/QuantitySelect.jsx
@@ -1,41 +1,33 @@
 import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
-const QuantityOptions = ({ optionsCount, quantity, onSelectQuantity }) => {
-  const menuItems = [...Array(optionsCount)].map((_, i) => (
+const OPTIONS_COUNT = 30;
+
+const QuantitySelect = ({ quantity, onSelectQuantity }) => {
+  const menuItems = [...Array(OPTIONS_COUNT)].map((_, i) => (
     <MenuItem value={i + 1} key={i + 1}>
       {i + 1}
     </MenuItem>
   ));
 
   return (
-    <Select
-      labelId="quantity"
-      id="quantity-select"
-      value={quantity}
-      onChange={(e) => onSelectQuantity(e.target.value)}
-      label="Quantity"
-      MenuProps={{
-        PaperProps: {
-          style: {
-            maxHeight: 200,
-          },
-        },
-      }}
-    >
-      {menuItems}
-    </Select>
-  );
-};
-
-const QuantitySelect = ({ quantity, onSelectQuantity }) => {
-  return (
     <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }} fullWidth>
       <InputLabel id="quantity">Quantity</InputLabel>
-      <QuantityOptions
-        optionsCount={30}
-        quantity={quantity}
-        onSelectQuantity={onSelectQuantity}
-      />
+      <Select
+        labelId="quantity"
+        id="quantity-select"
+        value={quantity}
+        onChange={(e) => onSelectQuantity(e.target.value)}
+        label="Quantity"
+        MenuProps={{
+          PaperProps: {
+            style: {
+              maxHeight: 200,
+            },
+          },
+        }}
+      >
+        {menuItems}
+      </Select>
     </FormControl>
   );
 };
